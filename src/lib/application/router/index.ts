e6d220d9aace5db4da472225101db6b79d5081e4
Yorri <zadlo.rafal.dev@gmail.com>
2025-04-13 15:46:23 +0200

    Lab4 begin

--- src/lib/application/router/index.ts
+++ src/lib/application/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/lib/presentation/views/HomeView.vue'
+import LoginPage from '@/lib/presentation/views/LoginPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
+      name: 'login',
+      component: LoginPage,
+    },
+    {
+      path: '/home',
       name: 'home',
       component: HomeView,
     },
